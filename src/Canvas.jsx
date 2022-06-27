5757568627d4996f19d91bfde44bee877d6df223
Corrie Grimshaw <corrie.e.grimshaw@gmail.com>
2022-06-27 07:26:35 -0700

    Created state on Canvas governing number of draggables on screen

--- src/Canvas.jsx
+++ src/Canvas.jsx
@@ -1,32 +1,47 @@
-import React, { useState } from "react";
+import React from "react";
 
 import Draggable from "./Draggable.jsx"
 import DropTarget from "./DropTarget.jsx"
 import AddButton from "./AddButton.jsx"
 
 
-function Canvas(props){
-	const [children, setChildren] = useState([1, 2, 3]);
-	const add = () => {
-		console.log("add")
-		setChildren((prevState)=> [...prevState, 1])
-		console.log(children.length);
+class Canvas extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = {children: []}
 	}
 
-	return (
-		<div id="canvas">
-		    <div id="morph-container">
-			<AddButton clickHandler={add}/>
-		 	<DropTarget id="circle"/>
-		 	<DropTarget id="color"/>
-		 	<DropTarget id="size"/>
-		 	<p id="text">click "+" to create shape. drag shape over boxes to morph</p>
-		 	</div>
-			{children.map((child, index) => <Draggable key={index}/>)}
-		</div>
-	)
-}
+	componentDidMount(){
+		console.log("component did mount");
+	}
 
+	componentWillUnmount(){
+		console.log("component will unmount")
+	}
+
+	add(){
+		this.setState((state)=> ({
+			children: [...state.children, <Draggable/>]
+		}))
+	}
+
+	render(){
+		return (
+			<div id="canvas">
+				<div id="morph-container">
+				<AddButton clickHandler={()=>{
+					this.add()
+				}}/>
+				<DropTarget id="circle"/>
+				<DropTarget id="color"/>
+				<DropTarget id="size" />
+				<p id="text">click "+" to create shape. drag shape over boxes to morph</p>
+				</div>
+				{this.state.children}
+			</div>
+		)
+	}
+}
 export default Canvas;
 
 
